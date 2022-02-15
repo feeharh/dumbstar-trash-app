3576c3ca9dad33af6a919d9c16163ec935f7846d
Adepoju Rofiat <finzy@Adepojus-MBP.attlocal.net>
2022-02-15 17:36:11 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DUMBSTAR-TRASH-APP',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
